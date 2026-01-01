1729ccbfc5ef6b33a83309c8a309e30f43ba0f1d
Injamam I <injamam@Injamams-MacBook-Air.local>
2026-01-01 14:51:47 +0530

    updated Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -17,14 +17,26 @@ class Pattern {
         System.out.println();
         }
     }
+    
+    public void pattern3(int n){
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+    }
 
     public static void main (String[]args){
 
     Pattern sol = new Pattern();
-    int n = 4;
+    int n = 5;
     sol.pattern1(n);
     System.out.println();
     sol.pattern2(n);
+    System.out.println();
+    sol.pattern3(n);
+
     }
 
 }
