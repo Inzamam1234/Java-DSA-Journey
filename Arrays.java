6846eff686940a74877571ef0df22f3b0748c506
Injamam <injamami85@gmail.com>
2026-02-16 22:05:49 +0530

    Started Arrays

--- Arrays.java
+++ Arrays.java
@@ -1,14 +1,58 @@
 import java.util.Scanner;
 
 public class Arrays {
+
+    public static int LinearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void ReverseArray(int[] arr) {
+
+        int start = 0;
+        int end = arr.length - 1; // Two pointers
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end]; // Swap
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the No of elements for the array : ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
+        }
+
+        /*
+         * int target = 5; //Linear Search
+         * 
+         * int result = LinearSearch(arr, target);
+         * 
+         * if(result != -1){
+         * System.out.println("Element found at index : " + result);
+         * }
+         * else{
+         * System.out.println("Element Not found !!");
+         * }
+         */
+        ReverseArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
         // for(int i = 0; i < arr.length; i++){
@@ -29,7 +73,17 @@ public class Arrays {
          * }
          */
 
+        /*
+         * int min = arr[0];
+         * for(int i = 0; i < arr.length; i++){
+         * if(arr[i] < min){ //Maximum element in an array
+         * min = arr[i];
+         * }
+         * }
+         */
+
         // System.out.println("Maximum element : " + max);
+        // System.out.println("Minimium element : " + min);
         // System.out.println("Sum of an array is : " + sum);
 
     }
