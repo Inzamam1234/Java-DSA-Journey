b257100e16cae9a718dcb6c7753a7ec8929a58c1
Injamam <injamami85@gmail.com>
2026-07-23 20:22:01 +0530

    Slinding Window

--- SlidingWindow/Main.java
+++ SlidingWindow/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static double findMaxAverage(int[] nums, int k) { // Fixed Window sliding Window pattern
 
         int curr_sum = 0;
-        
-        for(int i = 0; i < k; i++){  // First window
+
+        for (int i = 0; i < k; i++) { // First window
             curr_sum += nums[i];
         }
 
@@ -14,7 +14,7 @@ public class Main {
 
         int left = 0;
 
-        for(int i = k; i < nums.length; i++){ // Slide the window
+        for (int i = k; i < nums.length; i++) { // Slide the window
             curr_sum = curr_sum - nums[left] + nums[i];
             left++;
             max_sum = Math.max(max_sum, curr_sum);
@@ -23,9 +23,51 @@ public class Main {
         return (double) max_sum / k;
 
     }
+
+    private boolean isVowel(char c) {
+        return c == 'a' ||
+                c == 'e' ||
+                c == 'i' ||
+                c == 'o' ||
+                c == 'u';
+    }
+
+    public int maxVowels(String s, int k) {
+
+        int vowel_count = 0;
+
+        for (int i = 0; i < k; i++) { // First Window
+
+            if (isVowel(s.charAt(i))) {
+                vowel_count++;
+            }
+        }
+
+        int max_count = vowel_count;
+
+        int left = 0;
+
+        for (int i = k; i < s.length(); i++) { // Sliding Window
+
+            if (isVowel(s.charAt(left))) { // leaving element
+                vowel_count--;
+            }
+
+            if (isVowel(s.charAt(i))) { // incoming element
+                vowel_count++;
+            }
+
+            left++;
+
+            max_count = Math.max(max_count, vowel_count);
+        }
+
+        return max_count;
+    }
+
     public static void main(String[] args) {
-        
-        int nums[] = {2,4,5,7,8,7,6,5,4};
+
+        int nums[] = { 2, 4, 5, 7, 8, 7, 6, 5, 4 };
         System.out.println(findMaxAverage(nums, 3));
     }
 }
