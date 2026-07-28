331618380efbccd8f7caa9fbb32781b7cbf16ad1
Injamam <injamami85@gmail.com>
2026-07-28 16:01:06 +0530

    Stack problem

---
+++ Stack/Main.java
@@ -0,0 +1,34 @@
+package Stack;
+import java.util.*;
+public class Main {
+
+    public static boolean validParentheses(String s){
+
+        Stack <Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                    stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        
+        String s = "{([])}";
+
+        System.out.println(validParentheses(s));
+    }
+}
